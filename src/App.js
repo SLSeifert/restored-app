bf53242225fea974266743aae27b17add3f08256
SLSeifert <slseifert1986@gmail.com>
2026-03-04 23:12:08 -0800

    Install and experiment with sass.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import posts from "./postData";
 import Post from "./Post";
+import './main.scss';
 
 function App() {
   return (
     <div>
-      <h1>Restored Workshop Message Board</h1>
+      <h1 className="text-center title-text">
+        Restored Workshop Message Board
+      </h1>
       {posts.map((post) => (
         <Post post={post}></Post>
       ))}
